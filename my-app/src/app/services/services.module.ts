86d60fe363f93792699802d563ab8df01b164ec9
Morgane Bentzinger <mo.bentzinger@gmail.com>
2022-12-28 14:19:26 -0500

    create highlight in services page

--- my-app/src/app/services/services.module.ts
+++ my-app/src/app/services/services.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesHomeComponent } from './services-home/services-home.component';
+import { HighlightDirective } from './services-home/highlight.directive';
 
 
 @NgModule({
-  declarations: [ServicesHomeComponent],
+  declarations: [ServicesHomeComponent, HighlightDirective],
   imports: [
     CommonModule,
     ServicesRoutingModule
