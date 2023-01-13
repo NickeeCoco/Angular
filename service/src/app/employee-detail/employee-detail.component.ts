79dc7d65e9c0feb45ec25ace1b9fb3356209c986
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-01-12 21:55:10 -0500

    employee service

--- service/src/app/employee-detail/employee-detail.component.ts
+++ service/src/app/employee-detail/employee-detail.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-detail.component.css']
 })
 export class EmployeeDetailComponent {
-  public employees = [
-    {id: 1, "name": "Andrew", "age": 30},
-    {id: 2, "name": "Brandon", "age": 25},
-    {id: 3, "name": "Christina", "age": 26},
-    {id: 4, "name": "Elena", "age": 28},
-  ];
+  public employees = [];
 }
--- service/src/app/employee-list/employee-list.component.ts
+++ service/src/app/employee-list/employee-list.component.ts
@@ -6,12 +6,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent {
-
-  public employees = [
-    {id: 1, "name": "Andrew", "age": 30},
-    {id: 2, "name": "Brandon", "age": 25},
-    {id: 3, "name": "Christina", "age": 26},
-    {id: 4, "name": "Elena", "age": 28},
-  ];
-
+  public employees = [];
 }
